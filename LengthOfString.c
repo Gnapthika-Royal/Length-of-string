bac2af2f1e91d1a6b21893a274e6db702d3e623e
Gnapthika-Royal <gnapthika9@gmail.com>
2025-11-28 23:39:55 +0530

    Add files via upload

---
+++ LengthOfString.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[] = "Gnapthika";
+
+    // Find length of string using strlen()
+	printf("%lu", strlen(s));
+  
+    return 0;
+}
